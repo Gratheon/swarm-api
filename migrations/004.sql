9da188d5129e32b6e9b671f1305b086228c371d7
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-06-24 14:05:00 +0300

    add indexes

---
+++ migrations/004.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `frames_sides` ADD INDEX (`user_id`);
+ALTER TABLE `frames` ADD INDEX (`user_id`, `box_id`);
+ALTER TABLE `apiaries` ADD INDEX (`user_id`, `active`);
+ALTER TABLE `boxes` ADD INDEX (`user_id`, `hive_id`, `active`);
+ALTER TABLE `hives` ADD INDEX (`user_id`, `apiary_id`, `active`);
+ALTER TABLE `inspections` ADD INDEX (`user_id`, `hive_id`);
